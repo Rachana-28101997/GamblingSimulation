a3c7a9af734883aee58913c7e3f840a68ad02dcf
Rachana <rachanarrachu28@gmail.com>
2020-10-26 21:43:34 +0530

    Added : Random number genarated to check whether the gambler won or loss

--- src/com/bdlz/gambler/GamblingSimulation.java
+++ src/com/bdlz/gambler/GamblingSimulation.java
@@ -1,6 +1,19 @@
 package com.bdlz.gambler;
 
+import java.util.Random;
+
 public class GamblingSimulation {
 public static final int STAKE=100;
 public static final int BET=1;
+
+public void winOrLoss() {
+	Random random = new Random();
+	int value=random.nextInt(2);
+	if(value==1) {
+		System.out.println("Gambler won ");
+	}
+	else {
+		System.out.println("Gambler lost");
+	}
+}
 }
