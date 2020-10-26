fd3439f9ae14739982e7e5e1a45316d5b1755136
Rachana <rachanarrachu28@gmail.com>
2020-10-26 23:45:33 +0530

    Added : Each month winning and losing with count using hash map

--- src/com/bdlz/gambler/GamblingSimulation.java
+++ src/com/bdlz/gambler/GamblingSimulation.java
@@ -1,21 +1,24 @@
 package com.bdlz.gambler;
 
+import java.util.LinkedHashMap;
 import java.util.Random;
 
 public class GamblingSimulation {
 	public static final int STAKE = 100;
 	public static final int BET = 1;
 	public static final float STAKE_VALUE = 0.5f;
-	public int winningAmount, losingAmount, stake, totalAmountEarned;
+	public static int winningAmount, losingAmount, stake, totalAmountEarned;
+	static LinkedHashMap<Integer, Integer> daysWon = new LinkedHashMap<>();
+	static LinkedHashMap<Integer, Integer> daysLoss = new LinkedHashMap<>();
 
 	public void winOrLoss() {
 		Random random = new Random();
 		int value = random.nextInt(2);
 		if (value == 1) {
-			System.out.println("Gambler won ");
+			// System.out.println("Gambler won ");
 			stake++;
 		} else {
-			System.out.println("Gambler lost");
+			// System.out.println("Gambler lost");
 			stake--;
 		}
 	}
@@ -28,9 +31,11 @@ public class GamblingSimulation {
 		while (stop == true) {
 			winOrLoss();
 			if (stake == losingAmount) {
+				daysLoss.put(day, stake);
 				stop = false;
 			}
 			if (stake == winningAmount) {
+				daysWon.put(day, stake);
 				stop = false;
 			}
 		}
@@ -54,8 +59,35 @@ public class GamblingSimulation {
 		return totalAmountEarned;
 	}
 
+	public int calculateForMonth() {
+
+		getTotalAmountWonOrLoss();
+		System.out.println("****Days Won in  month****");
+		printDaysWithValue(daysWon);
+		System.out.println("****Days Loss in  month****");
+		printDaysWithValue(daysLoss);
+
+		return totalAmountEarned;
+		
+	}
+
+	public void printDaysWithValue(LinkedHashMap<Integer, Integer> daysValue) {
+		daysValue.forEach(((day, value) -> {
+			System.out.println("Day:- " + day + " Earned:- " + value);
+		}));
+	}
+
+	//public static void luckiestDay(LinkedHashMap<Integer, Integer> daysValue) {
+		//daysValue.forEach(((day,value)->{
+			//System.out.println("the lucciest day" +Math.max(day, stake)+ +day+ " " +stake);}));
+		//}
+
+	
+
 	public static void main(String[] agrs) {
 		GamblingSimulation simulation = new GamblingSimulation();
-		System.out.println(simulation.resignStake(20));
+		// System.out.println(simulation.resignStake(20));
+		simulation.calculateForMonth();
+		//GamblingSimulation.luckiestDay(daysWon);
 	}
 }
